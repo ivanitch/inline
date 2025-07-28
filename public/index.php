34f9679f05c114a302ea3341327fcbd2a403de3b
Sergey Ivanov <ivanitch.mail@gmail.com>
2025-07-28 17:15:28 +0900

    Docker + Composer

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+// Develop
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+(new \App\App())->run();
